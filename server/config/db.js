ac55d20d6d6562b4487fbd87cb78ac39287cbb72
muhurtkumar <muhurtkumar@gmail.com>
2025-06-27 02:38:08 +0530

    Added vercel.json configuration for deploying Express server on Vercel

--- server/config/db.js
+++ server/config/db.js
@@ -3,7 +3,7 @@ import mongoose from  "mongoose";
 // Function to connect to the MongoDB database
 const connectDB = async () => {
     mongoose.connection.on('connected', () => console.log('Database connected successfully'));
-    await mongoose.connect(`${process.env.MONGODB_URI}`)
+    await mongoose.connect(`${process.env.MONGODB_URI}/joborbit`)
 }
 
 export default connectDB
