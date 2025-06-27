b797e14da27339898efdd55177014c2c1d5f82a8
muhurtkumar <muhurtkumar@gmail.com>
2025-06-28 02:58:51 +0530

    Implemented company registration controller with password hashing, image upload, and JWT token

--- server/controllers/companyController.js
+++ server/controllers/companyController.js
@@ -1,7 +1,53 @@
+import Company from "../models/Company.js";
+import bcrypt from 'bcrypt';
+import {v2 as cloudinary} from 'cloudinary';
+import generateToken from "../utils/generateToken.js";
 
 // Register a new company
 export const registerCompany = async (req, res) => {
+    const { name, email, password } = req.body;
+    const imageFile = req.file;
 
+    if(!name || !email || !password || !imageFile) {
+        return res.status(400).json({success:false, message: "All fields are required" });
+    }
+
+    try {
+        const companyExist = await Company.findOne({ email });
+        if(companyExist) {
+            return res.status(400).json({success:false, message: "Company already registered" });
+        }
+
+        const salt = await bcrypt.genSalt(10)
+        const hashPassword = await bcrypt.hash(password, salt);
+
+        const imageUpload = await cloudinary.uploader.upload(imageFile.path)
+
+        const company = await Company.create({
+            name,
+            email,
+            password: hashPassword,
+            image: imageUpload.secure_url
+        })
+
+        res.status(201).json({
+            success: true,
+            message: "Company registered successfully",
+            company: {
+                id: company._id,
+                name: company.name,
+                email: company.email,
+                image: company.image
+            },
+            token: generateToken(company._id)
+        });
+
+    } catch (error) {
+        res.json({
+            success: false,
+            message: error.message
+        })
+    }
 }
 
 // Company Login
