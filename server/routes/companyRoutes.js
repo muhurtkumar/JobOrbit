a1505370367dba12fa0b2d697f5d131eed4d4894
muhurtkumar <muhurtkumar@gmail.com>
2025-06-28 01:28:15 +0530

    Set up company routes and controller skeletons for comapny, jobs and applicants management

---
+++ server/routes/companyRoutes.js
@@ -0,0 +1,30 @@
+import express from 'express';
+import { changeJobApplicationsStatus, changeVisibility, getCompanyData, getCompanyJobApplicants, getCompanyPostedJobs, loginCompany, postJob, registerCompany } from '../controllers/companyController.js';
+
+const router = express.Router();
+
+// Register a company
+router.post('/register', registerCompany)
+
+// Login a company
+router.post('/login', loginCompany)
+
+// Get company data
+router.get('/company', getCompanyData)
+
+// Post a job
+router.post('/post-job', postJob)
+
+// Get Applicants data of company
+router.get('/applicants', getCompanyJobApplicants)
+
+// Get Posted Jobs of company
+router.get('/list-jobs', getCompanyPostedJobs)
+
+// Change Job Application Status
+router.post('/change-status', changeJobApplicationsStatus)
+
+// Change Job Visibility
+router.post('/change-visibility', changeVisibility)
+
+export default router;
--- server/server.js
+++ server/server.js
@@ -5,6 +5,7 @@ import 'dotenv/config';
 import connectDB from './config/db.js';
 import * as Sentry from "@sentry/node";
 import { clerkWebhooks } from './controllers/webhooks.js';
+import companyRoutes from './routes/companyRoutes.js';
 
 // Initialize the Express application
 const app = express();
@@ -22,6 +23,7 @@ app.get("/debug-sentry", function mainHandler(req, res) {
   throw new Error("My first Sentry error!");
 });
 app.post('/webhooks', clerkWebhooks);
+app.use('/api/company', companyRoutes);
 
 //PORT
 const PORT = process.env.PORT || 5000;
