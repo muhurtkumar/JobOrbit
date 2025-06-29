f54657f4417df4e15e5d1a93093137ffdc4e6462
muhurtkumar <muhurtkumar@gmail.com>
2025-06-30 03:11:26 +0530

    Added user routes and controller skeleton for user data, job applications, and resume update

---
+++ server/controllers/userController.js
@@ -0,0 +1,20 @@
+
+// Get user data
+export const getUserData = async (req, res) => {
+    
+}
+
+// Apply for a job
+export const applyForJob = async (req, res) => {
+
+}
+
+// Get user applied jobs
+export const getUserJobApplications = async (req, res) => {
+
+}
+
+// Update user profile (resume)
+export const updateUserResume = async (req, res) => {
+
+}
