3814eb20f18c8c9dc26b627ca4421d4e5a94c025
muhurtkumar <muhurtkumar@gmail.com>
2025-06-30 03:12:52 +0530

    Setup Clerk middleware in Express app for user authentication and implemented function to fetch user details

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,7 +1,20 @@
+import User from "../models/User.js"
 
 // Get user data
 export const getUserData = async (req, res) => {
-    
+    const userId = req.auth.userId
+
+    try {
+        const user = await User.findById(userId)
+
+        if(!user) {
+            return res.status(404).json({success: false, message: "User not found!"})
+        }
+
+        res.json({success: true, user})
+    } catch (error) {
+        res.json({success: false, message: error.message})
+    }
 }
 
 // Apply for a job
