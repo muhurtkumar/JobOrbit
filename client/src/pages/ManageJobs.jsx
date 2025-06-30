dbb2cb7a6dce5c5befe18510c2a7d37cf1e99c3c
muhurtkumar <muhurtkumar@gmail.com>
2025-07-01 02:27:34 +0530

    Integrated API to fetch company-specific job listings and added visibility toggle functionality with backend update

--- client/src/pages/ManageJobs.jsx
+++ client/src/pages/ManageJobs.jsx
@@ -2,10 +2,63 @@ import React from 'react'
 import { manageJobsData } from '../assets/assests'
 import moment from 'moment'
 import { useNavigate } from 'react-router-dom'
+import { useState } from 'react'
+import { useContext } from 'react'
+import { AppContext } from '../context/AppContext'
+import axios from 'axios'
+import { toast } from 'react-toastify'
+import { useEffect } from 'react'
 
 const ManageJobs = () => {
 
+    const {backendUrl, companyToken} = useContext(AppContext);
     const navigate = useNavigate();
+
+    const [jobs, setJobs] = useState([]);
+
+    // Function to fetch company job Applications data
+    const fetchCompanyJobs = async () => {
+        try {
+            const {data} = await axios.get(backendUrl + '/api/company/list-jobs',
+                {headers: {token: companyToken}}
+            )
+
+            if(data.success){
+                setJobs(data.jobsData.reverse());
+                console.log(data.jobsData);
+            }
+            else{
+                toast.error(data.message);
+            }
+        } catch (error) {
+            toast.error(error.message);
+        }
+    }
+
+    // Function to change Job Visibility
+    const changeJobVisibility = async (id) => {
+        try {
+            const {data} = await axios.post(backendUrl + '/api/company/change-visibility',
+                {id},
+                {headers: {token: companyToken}}
+            )
+            if(data.success){
+                toast.success(data.message);
+                fetchCompanyJobs(); // Refresh the job list after changing visibility
+            }
+            else{
+                toast.error(data.message);
+            }
+        } catch (error) {
+            toast.error(error.message);
+        }
+    }
+
+    useEffect(() => {
+        if(companyToken){
+            fetchCompanyJobs();
+        }
+    }, [companyToken])
 
     return (
         <div className='container p-4 max-w-5xl'>
@@ -22,7 +75,7 @@ const ManageJobs = () => {
                         </tr>
                     </thead>
                     <tbody>
-                        {manageJobsData.map((job, index) => (
+                        {jobs.map((job, index) => (
                             <tr key ={index} className='text-gray-700'>
                                 <td className='py-2 px-4 border-b max-sm:hidden'>{index+1}</td>
                                 <td className='py-2 px-4 border-b'>{job.title}</td>
@@ -30,7 +83,7 @@ const ManageJobs = () => {
                                 <td className='py-2 px-4 border-b max-sm:hidden'>{job.location}</td>
                                 <td className='py-2 px-4 border-b text-center'>{job.applicants}</td>
                                 <td className='py-2 px-4 border-b'>
-                                    <input className='scale-125 ml-4' type="checkbox" />
+                                    <input onChange={()=> changeJobVisibility(job._id)} className='scale-125 ml-4' type="checkbox" checked={job.visible}/>
                                 </td>
                             </tr>
                         ))}
